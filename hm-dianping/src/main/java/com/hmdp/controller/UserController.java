b1946b2338a99ac4dce5b1bf9e58ff68ec38de4c
dezhizhang <1541609448@qq.com>
2024-07-15 21:32:51 +0800

    feat:获取用户信息

--- hm-dianping/src/main/java/com/hmdp/controller/UserController.java
+++ hm-dianping/src/main/java/com/hmdp/controller/UserController.java
@@ -6,7 +6,9 @@ import com.hmdp.dto.Result;
 import com.hmdp.entity.UserInfo;
 import com.hmdp.service.IUserInfoService;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.UserHolder;
 import lombok.extern.slf4j.Slf4j;
+import com.hmdp.entity.User;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -45,8 +47,6 @@ public class UserController {
      */
     @PostMapping("/login")
     public Result login(@RequestBody LoginFormDTO loginForm, HttpSession session){
-        System.out.println("----------");
-        System.out.println(loginForm);
         return userService.login(loginForm,session);
     }
 
@@ -62,8 +62,8 @@ public class UserController {
 
     @GetMapping("/me")
     public Result me(){
-        // TODO 获取当前登录的用户并返回
-        return Result.fail("功能未完成");
+        User user = UserHolder.getUser();
+        return Result.ok(user);
     }
 
     @GetMapping("/info/{id}")
