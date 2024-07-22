17abde50e59705d4ce24cd16109956d457590176
dezhizhang <1541609448@qq.com>
2024-07-22 17:39:56 +0800

    feat: 依赖倒置

--- design/src/principle/SingleResponse.java
+++ design/src/principle/SingleResponse.java
@@ -4,11 +4,22 @@ public class SingleResponse {
     public static void main(String[] args) {
         Vehicle vehicle = new Vehicle();
         vehicle.run("小汔车");
+        vehicle.runAir("飞机");
+        vehicle.runWater("轮船");
     }
 }
 
 class Vehicle {
-    public void run(String vehicle) {
-        System.out.println(vehicle + "在公路上运行");
-    }
+   public void run(String vehicleName) {
+       System.out.println(vehicleName + "在公路上运行...");
+   }
+
+   public void runAir(String vehicleName) {
+       System.out.println(vehicleName + "在天空飞行...");
+   }
+
+   public void runWater(String vehicleName) {
+       System.out.println(vehicleName + "在水中运行");
+   }
+
 }
---
+++ design/src/shop/xiaozhi/inversion/DependInversion.java
@@ -0,0 +1,38 @@
+package shop.xiaozhi.inversion;
+
+public  class DependInversion {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.receive(new Email());
+
+        person.receive(new WeiXin());
+    }
+}
+
+interface  IReceiver{
+    public String getInfo();
+}
+
+class Email implements IReceiver{
+    @Override
+    public String getInfo() {
+        return "电子邮箱信息：1541609448@qq.com";
+    }
+}
+
+class WeiXin implements IReceiver{
+    @Override
+    public String getInfo() {
+        return "微信信息：15992478448";
+    }
+}
+
+class Person{
+    public void receive(IReceiver receiver){
+        System.out.println(receiver.getInfo());
+    }
+}
+
+
+
+
