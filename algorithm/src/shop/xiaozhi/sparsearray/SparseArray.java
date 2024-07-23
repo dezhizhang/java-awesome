7aea7321c846570e606a777d6b3d192106abaf00
dezhizhang <1541609448@qq.com>
2024-07-23 23:08:38 +0800

    feat:稀疏数组

---
+++ algorithm/src/shop/xiaozhi/sparsearray/SparseArray.java
@@ -0,0 +1,58 @@
+package shop.xiaozhi.sparsearray;
+
+public class SparseArray {
+    public static void main(String[] args) {
+        // 创建一个原始的二维数组11*11
+        //0：表示没有棋子，1：表示黑子，2示不白子
+        int chessArr[][] = new int[11][11];
+        chessArr[1][2] = 1;
+        chessArr[2][3] = 2;
+
+        for(int[] row:chessArr) {
+            for(int col:row) {
+                System.out.printf("%d\t",col);
+            }
+            System.out.println();
+        }
+
+        //1 将二维数组转换成稀疏数组
+        //2 先遍历二维数组，得到非0数据个数
+        int sum = 0;
+        for(int i =0;i < chessArr.length;i++) {
+            for(int j =0;j < chessArr[i].length;j++) {
+                if(chessArr[i][j] !=0) {
+                    sum++;
+                }
+            }
+        }
+
+        //3 创建对应的稀疏数组
+        int spareArr[][] = new int[sum + 1][3];
+        //4 给稀疏数赋值
+        spareArr[0][0] = chessArr.length;
+        spareArr[0][1] = chessArr.length;
+        spareArr[0][2] = sum;
+
+        int count = 0;
+
+        for(int i =0;i < chessArr.length;i++) {
+            for(int j =0;j < chessArr[i].length;j++) {
+                if(chessArr[i][j] !=0) {
+                    count++;
+                    spareArr[count][0] = i;
+                    spareArr[count][1] = j;
+                   spareArr[count][2] = chessArr[i][j];
+                }
+            }
+        }
+
+        System.out.println("得到的稀疏数组");
+        for(int[] row:spareArr) {
+            for(int col:row) {
+                System.out.printf("%d\t",col);
+            }
+            System.out.println();
+        }
+
+    }
+}
