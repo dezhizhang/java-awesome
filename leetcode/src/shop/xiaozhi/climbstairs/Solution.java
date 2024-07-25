76866ae0c61c807683ad470a92e54e25b8460548
dezhizhang <1541609448@qq.com>
2024-07-25 14:03:54 +0800

    feat:climbStairs

---
+++ leetcode/src/shop/xiaozhi/climbstairs/Solution.java
@@ -0,0 +1,31 @@
+package shop.xiaozhi.climbstairs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+//每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+
+//https://leetcode.cn/problems/climbing-stairs/description/
+public class Solution {
+    private static final Map<Integer, Integer> storeMap = new HashMap<>();
+
+    public static void main(String[] args) {
+        int res = climbStairs(12);
+        System.out.println(res);
+
+    }
+
+    public static int climbStairs(int n) {
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+
+        if(null != storeMap.get(n)) {
+            return storeMap.get(n);
+        }else {
+            int result = climbStairs(n - 1) + climbStairs(n - 2);
+            storeMap.put(n,result);
+            return result;
+        }
+    }
+}
