e7c2236b2199b8cd7026e2ec140dadce7302969d
dezhizhang <1541609448@qq.com>
2024-08-10 21:55:56 +0800

    feat:前序查找中序查找

--- algorithm/src/shop/xiaozhi/tree/BinaryTreeTest.java
+++ algorithm/src/shop/xiaozhi/tree/BinaryTreeTest.java
@@ -132,50 +132,49 @@ class HeroNode {
         if (this.no == no) {
             return this;
         }
-        // 判断当前节点的左子树是否为空，如果不为空，则递归前序查找
         HeroNode resNode = null;
+        // 判断当前左子树是否为空如果不为空则递当查找
         if (this.left != null) {
             resNode = this.left.preOrderSearch(no);
         }
         if (resNode != null) {
-            // 左子树找到节点
             return resNode;
         }
-        // 当前节点右子树是为空为，如果不为空则向右递归查找
+        // 向右子树递归查找
         if (this.right != null) {
             resNode = this.right.preOrderSearch(no);
         }
-
         return resNode;
     }
 
     // 中序遍历查找
     public HeroNode infixOrderSearch(int no) {
         HeroNode resNode = null;
-        // 判断当前左子节点是否为空，如果不为空，则递归中序查找
+        // 判断当前左子节点是否为空，如果不为空则递归中序查找
         if (this.left != null) {
             resNode = this.left.infixOrderSearch(no);
         }
-        // 如果找到就直接返回没有找到比较当前节点
+        // 如果找到就返回否则比较当前节点
         if (resNode != null) {
             return resNode;
         }
 
+        // 比较当前节点
         if (this.no == no) {
             return this;
         }
 
+        // 否则比较右子树
         if (this.right != null) {
             resNode = this.right.infixOrderSearch(no);
         }
-
         return resNode;
     }
 
     // 后序遍历查找
     public HeroNode postOrderSearch(int no) {
         HeroNode resNode = null;
-        // 判断当前左子树是否为空，如果不为空，则递归后序查找
+        // 判断当前左子树是否为空，如果不为空则递归后序查找
         if (this.left != null) {
             resNode = this.left.postOrderSearch(no);
         }
@@ -183,7 +182,8 @@ class HeroNode {
         if (resNode != null) {
             return resNode;
         }
-        // 判断右子杩是否为空
+
+        // 判断右树是否为空
         if (this.right != null) {
             resNode = this.right.postOrderSearch(no);
         }
@@ -192,7 +192,7 @@ class HeroNode {
             return resNode;
         }
 
-        // 左右子树都没有找到比较当前节点
+        //  左右子树都没有找到比较当前节点
         if (this.no == no) {
             return this;
         }
