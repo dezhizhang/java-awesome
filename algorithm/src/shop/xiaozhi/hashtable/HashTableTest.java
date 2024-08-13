ee8ae5a913c61d0670e70bfe1ca39e8295094c82
dezhizhang <1541609448@qq.com>
2024-08-13 21:40:40 +0800

    feat:哈希表

--- algorithm/src/shop/xiaozhi/hashtable/HashTableTest.java
+++ algorithm/src/shop/xiaozhi/hashtable/HashTableTest.java
@@ -13,9 +13,10 @@ public class HashTableTest {
         table.put(1, "zhangsan", "张皿");
         System.out.println(table.table[1].value);
 
-        table.remove(17,"lisi");
+        table.remove(17, "lisi");
 
         System.out.println(table.size);
+        System.out.println(table.get(1, "zhangsan"));
         System.out.println(table.table[1].value);
         System.out.println(table.table[1].next.value);
     }
@@ -44,6 +45,8 @@ class HashTable {
 
     Entry[] table = new Entry[16];
     int size = 0; // 元素个数
+    float loadFactor = 0.75f;
+    int threshold = (int) (loadFactor * table.length);
 
     // 根据hash码获取value
     Object get(int hash, Object key) {
@@ -60,7 +63,6 @@ class HashTable {
             }
             p = p.next;
         }
-
         return null;
     }
 
@@ -85,6 +87,58 @@ class HashTable {
             p.next = new Entry(hash, key, value);
         }
         size++;
+        if (size > threshold) {
+            // 扩容
+            resize();
+        }
+    }
+
+    void resize() {
+        Entry[] newTable = new Entry[table.length << 1];
+
+        for (Entry entry : table) {
+            Entry p = entry;
+            if (p != null) {
+                Entry a = null;
+                Entry b = null;
+
+                Entry aHead = null;
+                Entry bHead = null;
+
+                while (p != null) {
+                    if ((p.hash & table.length) == 0) {
+                        if (a != null) {
+                            a.next = p;
+                        } else {
+                            aHead = p;
+                        }
+                        // 分配到a
+                        a = p;
+                    } else {
+                        if (b != null) {
+                            b.next = p;
+                        } else {
+                            bHead = p;
+                        }
+                        // 分配到b
+                        b = p;
+                    }
+                    p = p.next;
+                }
+
+                if (a != null) {
+                    a.next = null;
+                }
+
+                if (b != null) {
+                    b.next = null;
+                }
+
+            }
+        }
+
+        table = newTable;
+        threshold = (int) (loadFactor * table.length);
     }
 
     // 根据hash码删除
@@ -112,7 +166,6 @@ class HashTable {
         }
         return null;
     }
-
 }
 
 
