a6b9d44d5e93d9eaf6cbfc49676aa3f69ffbed56
dezhizhang <1541609448@qq.com>
2024-08-13 19:51:10 +0800

    feat:哈希表

--- algorithm/src/shop/xiaozhi/hashtable/HashTable.java
+++ algorithm/src/shop/xiaozhi/hashtable/HashTable.java
@@ -23,21 +23,6 @@ class Entry {
     }
 
     // 根据hash码获取value
-    public Object get(int hash, Object k) {
-        int idx = hash & (table.length - 1);
-        if (table[idx] == null) {
-            return null;
-        }
-        Entry p = table[idx];
-        while (p != null) {
-            if (p.key.equals(k)) {
-                return p.value;
-            }
-            p = p.next;
-        }
-        return null;
-    }
-
     // 向hash表存入新key value,如果key重复则更新value
     public void put(int hash, Object key, Object value) {
         int idx = hash & table.length - 1;
@@ -65,6 +50,31 @@ class Entry {
     }
 
 
+    public Object remove(int hash, Object key) {
+        int idx = hash & (table.length - 1);
+        if (table[idx] == null) {
+            return null;
+        }
 
+        Entry p = table[idx];
+        Entry prev = null;
+
+        while (p != null) {
+            if (p.key.equals(key)) {
+                // 找到删除
+                if (prev == null) {
+                    table[idx] = p.next;
+                } else {
+                    prev.next = p.next;
+                }
+                size--;
+                return p.value;
+            }
+            prev = p;
+            p = p.next;
+
+        }
+        return null;
+    }
 
 }
