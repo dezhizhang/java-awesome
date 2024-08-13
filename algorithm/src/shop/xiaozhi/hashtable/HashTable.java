06e2a1c73398ac1f6e13d0cb182526f8c7e8e880
dezhizhang <1541609448@qq.com>
2024-08-13 20:20:41 +0800

    feat:HashTable

--- algorithm/src/shop/xiaozhi/hashtable/HashTable.java
+++ algorithm/src/shop/xiaozhi/hashtable/HashTable.java
@@ -1,80 +1,40 @@
 package shop.xiaozhi.hashtable;
 
 public class HashTable {
-    public static void main(String[] args) {
+    // 节点类
+    static class Entry {
+        int hash; // 哈希码
+        Object key; // 键
+        Object value; // 值
+        Entry next;
+
+        public Entry(int hash, Object key, Object value) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+        }
 
 
     }
-}
 
-
-class Entry {
-    int hash; // 哈希码
-    Object key; // 键
-    Object value; // 值
-    Entry next;
-    int size = 0;
     Entry[] table = new Entry[16];
-
-    public Entry(int hash, Object key, Object value) {
-        this.hash = hash;
-        this.key = key;
-        this.value = value;
-    }
+    int size = 0; // 元素个数
 
     // 根据hash码获取value
-    // 向hash表存入新key value,如果key重复则更新value
-    public void put(int hash, Object key, Object value) {
-        int idx = hash & table.length - 1;
-
-        // idx处有空位直接新增
-        if (table[idx] == null) {
-            table[idx] = new Entry(hash, key, value);
-        } else {
-            Entry p = table[idx];
-            while (true) {
-                // 更新新节点
-                if (p.key.equals(key)) {
-                    p.value = value;
-                    return;
-                }
-                if (p.next == null) {
-                    break;
-                }
-                p = p.next;
-            }
-            // 添加新节点
-            p.next = new Entry(hash, key, value);
-        }
-        size++;
+    Object get(int hash, Object key) {
+        return null;
     }
 
+    // 根据hash存放数据，如果key重复更新value
+    void put(int hash, Object key, Object value) {
 
-    public Object remove(int hash, Object key) {
-        int idx = hash & (table.length - 1);
-        if (table[idx] == null) {
-            return null;
-        }
+    }
 
-        Entry p = table[idx];
-        Entry prev = null;
-
-        while (p != null) {
-            if (p.key.equals(key)) {
-                // 找到删除
-                if (prev == null) {
-                    table[idx] = p.next;
-                } else {
-                    prev.next = p.next;
-                }
-                size--;
-                return p.value;
-            }
-            prev = p;
-            p = p.next;
-
-        }
+    // 根据hash码删除
+    Object remove(int hash, Object key) {
         return null;
     }
 
 }
+
+
