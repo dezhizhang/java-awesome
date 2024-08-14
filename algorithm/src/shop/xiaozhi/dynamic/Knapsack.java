a8d2afdbfd42ff6c0db38d077334b73835c8af03
dezhizhang <1541609448@qq.com>
2024-08-14 21:49:15 +0800

    feat:匹配

--- algorithm/src/shop/xiaozhi/dynamic/Knapsack.java
+++ algorithm/src/shop/xiaozhi/dynamic/Knapsack.java
@@ -15,7 +15,6 @@ public class Knapsack {
         // 为了记录放入商品的情况，定义一个二维数组
         int[][] path = new int[n + 1][m + 1];
 
-
         // 初始化第一行和第一列
         for (int i = 0; i < v.length; i++) {
             v[i][0] = 0;
@@ -31,20 +30,11 @@ public class Knapsack {
                 } else {
                     if (v[i - 1][j] < val[i - 1] + v[i - 1][j - w[i - 1]]) {
                         v[i][j] = Math.max(v[i - 1][j], val[i - 1] + v[i - 1][j - w[i - 1]]);
-                        path[i][j] = 1;
                     } else {
                         v[i][j] = v[i - 1][j];
                     }
-
                 }
             }
-        }
-
-        for (int[] ints : v) {
-            for (int anInt : ints) {
-                System.out.print(anInt + " ");
-            }
-            System.out.println();
         }
 
         // 输出放入的那些商品
@@ -52,13 +42,13 @@ public class Knapsack {
         int j = path[0].length - 1;
         while (i > 0 && j > 0) {
             if (path[i][j] == 1) {
-                System.out.printf("第%d个商品放入到背包的%d\n", i,v[i][j]);
+                System.out.printf("装入的商品最大值%d", v[i][j]);
                 j -= w[i - 1];
                 break;
             }
             i--;
-        }
 
+        }
     }
 }
 
---
+++ algorithm/src/shop/xiaozhi/kmp/Violence.java
@@ -0,0 +1,40 @@
+package shop.xiaozhi.kmp;
+
+public class Violence {
+    public static void main(String[] args) {
+        String s1 = "hello world";
+        String s2 = "world";
+
+        int index = violence(s1, s2);
+
+        System.out.println(index);
+    }
+
+    public static int violence(String str1, String str2) {
+        char[] s1 = str1.toCharArray();
+        char[] s2 = str2.toCharArray();
+
+        int s1Len = s1.length;
+        int s2Len = s2.length;
+
+        int i = 0;
+        int j = 0;
+
+        while (i < s1Len && j < s2Len) {
+            if (s1[i] == s2[j]) {
+                i++;
+                j++;
+            } else {
+                // 设有匹配成功
+                i = i - (j - 1);
+                j = 0;
+            }
+        }
+
+        if (j == s2Len) {
+            return i - j;
+        }
+
+        return -1;
+    }
+}
