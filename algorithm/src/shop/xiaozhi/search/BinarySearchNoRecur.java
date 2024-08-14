98a3909ea0bafce6bc74e90b5aec4a65f663cf1f
dezhizhang <1541609448@qq.com>
2024-08-14 19:20:54 +0800

    feat:二分查找

--- algorithm/src/shop/xiaozhi/search/BinarySearchNoRecur.java
+++ algorithm/src/shop/xiaozhi/search/BinarySearchNoRecur.java
@@ -3,22 +3,22 @@ package shop.xiaozhi.search;
 public class BinarySearchNoRecur {
     public static void main(String[] args) {
         int[] arr = {1,3,8,10,11,67,100};
-        int index = binarySearch(arr, 3);
+        int index = binarySearch(arr, 67);
         System.out.println(index);
 
     }
 
-    public static int binarySearch(int[] arr, int target) {
+
+    public static int binarySearch(int[] arr,int target) {
         int left = 0;
         int right = arr.length - 1;
-        while (left <= right) {
+        while (left <=right) {
             int mid = (left + right) / 2;
-
-            if (arr[mid] == target) {
+            if(arr[mid] == target) {
                 return mid;
-            } else if (arr[mid] > target) {
+            }else if(arr[mid] > target) {
                 right = mid - 1;
-            } else {
+            }else {
                 left = mid + 1;
             }
         }
